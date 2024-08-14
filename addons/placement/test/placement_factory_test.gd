ed1f11cb93a896453bd63444fa493f526f25a09e
Matthew Underwood <farban6@gmail.com>
2024-08-14 22:27:34 +0100

    formatting

--- addons/placement/test/placement_factory_test.gd
+++ addons/placement/test/placement_factory_test.gd
@@ -4,9 +4,9 @@ extends GdUnitTestSuite
 
 func test_vertices():
     var keys = [
-        Vector2(1, 1),Vector2(1, 2),Vector2(1, 3),
-        Vector2(2, 1),Vector2(2, 2),Vector2(2, 3),
-        Vector2(3, 1),Vector2(3, 2),Vector2(3, 3)
+        Vector2(1, 1), Vector2(1, 2), Vector2(1, 3),
+        Vector2(2, 1), Vector2(2, 2), Vector2(2, 3),
+        Vector2(3, 1), Vector2(3, 2), Vector2(3, 3)
     ]
 
     var pos = Vector2(2, 2)
@@ -18,9 +18,9 @@ func test_vertices():
 
 func test_edges():
     var keys = [
-        Vector2(1, 1),Vector2(1, 2),Vector2(1, 3),
-        Vector2(2, 1),Vector2(2, 3),Vector2(3, 1),
-        Vector2(3, 2),Vector2(3, 3)
+        Vector2(1, 1), Vector2(1, 2), Vector2(1, 3),
+        Vector2(2, 1), Vector2(2, 3), Vector2(3, 1),
+        Vector2(3, 2), Vector2(3, 3)
     ]
 
     var pos = Vector2(2, 2)
@@ -32,7 +32,6 @@ func test_edges():
 
 func test_corners():
     var keys = [Vector2(1, 1), Vector2(1, 3), Vector2(3, 1), Vector2(3, 3)]
-
     var pos = Vector2(2, 2)
     var size = Vector2(3, 3)
     var placement = (MUP_Placement_Factory.new()).create(pos, size)
